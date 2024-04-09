490e7378ea607f73d129f1f60fa83b3ce86b2438
Pilzschaf <pilzschaf@t-online.de>
2024-04-09 20:15:15 +0200

    Resize working

--- examples/compute_clear/main.c
+++ examples/compute_clear/main.c
@@ -4,28 +4,27 @@
 
 #include <stromboli/stromboli.h>
 
+#ifndef MAX_FRAMES_IN_FLIGHT
+#define MAX_FRAMES_IN_FLIGHT 1
+#endif
+
 StromboliSwapchain swapchain;
 StromboliRenderpass renderPass;
 StromboliPipeline computePipeline;
 StromboliPipeline graphicsPipeline;
+StromboliRenderSection graphicsSection;
+StromboliRenderSection computeSection;
 
-VkCommandPool computeCommandPool;
-VkCommandBuffer computeCommandBuffer;
-VkCommandPool graphicsCommandPool;
-VkCommandBuffer graphicsCommandBuffer;
-VkSemaphore imageAcquireSemaphore;
+VkSemaphore imageAcquireSemaphores[MAX_FRAMES_IN_FLIGHT];
+VkSemaphore imageReleaseSemaphores[MAX_FRAMES_IN_FLIGHT];
 VkDescriptorPool descriptorPool;
 VkDescriptorSet descriptorSet;
 
-// Next time: Graphics pipeline
-// Renderpasses
-
-void resizeApplication(StromboliContext* context, u32 width, u32 height) {
-    vkDeviceWaitIdle(context->device);
-    stromboliSwapchainResize(context, &swapchain, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT, width, height);
-}
-
-void createResources(StromboliContext* context) {
+void recreateRenderpass(StromboliContext* context) {
+    if(renderPass.renderPass) {
+        stromboliRenderpassDestroy(context, &renderPass);
+        MEMORY_CLEAR_STRUCT(&renderPass);
+    }
     struct StromboliAttachment outputAttachments[] = {
         { // Color attachment
             .clearColor.color = (VkClearColorValue){{0.4f, 0.05f, 1.0f, 1.0f}},
@@ -43,37 +42,25 @@ void createResources(StromboliContext* context) {
         .swapchainOutput = &swapchain,
     };
     renderPass = stromboliRenderpassCreate(context, swapchain.width, swapchain.height, 1, &subpass);
+}
+
+void resizeApplication(StromboliContext* context, u32 width, u32 height) {
+    vkDeviceWaitIdle(context->device);
+    stromboliSwapchainResize(context, &swapchain, VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT | VK_IMAGE_USAGE_STORAGE_BIT, width, height);
+    recreateRenderpass(context);
+}
+
+void createResources(StromboliContext* context) {
+    recreateRenderpass(context);
     graphicsPipeline = stromboliPipelineCreateGraphics(context, &(struct StromboliGraphicsPipelineParameters) {
         .vertexShaderFilename = STR8_LITERAL("examples/compute_clear/triangle.vert.spv"),
         .fragmentShaderFilename = STR8_LITERAL("examples/compute_clear/triangle.frag.spv"),
         .renderPass = renderPass.renderPass,
     });
-
     computePipeline = stromboliPipelineCreateCompute(context, STR8_LITERAL("examples/compute_clear/clear.comp.spv"));
-    {
-        VkCommandPoolCreateInfo createInfo = {VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
-        createInfo.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
-        createInfo.queueFamilyIndex = context->computeQueues[0].familyIndex;
-        vkCreateCommandPool(context->device, &createInfo, 0, &computeCommandPool);
 
-        VkCommandBufferAllocateInfo allocateInfo = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO};
-        allocateInfo.commandBufferCount = 1;
-        allocateInfo.commandPool = computeCommandPool;
-        allocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        vkAllocateCommandBuffers(context->device, &allocateInfo, &computeCommandBuffer);
-    }
-    {
-        VkCommandPoolCreateInfo createInfo = {VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO};
-        createInfo.flags = VK_COMMAND_POOL_CREATE_TRANSIENT_BIT;
-        createInfo.queueFamilyIndex = context->graphicsQueues[0].familyIndex;
-        vkCreateCommandPool(context->device, &createInfo, 0, &graphicsCommandPool);
-
-        VkCommandBufferAllocateInfo allocateInfo = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO};
-        allocateInfo.commandBufferCount = 1;
-        allocateInfo.commandPool = graphicsCommandPool;
-        allocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        vkAllocateCommandBuffers(context->device, &allocateInfo, &graphicsCommandBuffer);
-    }
+    graphicsSection = createRenderSection(context, &context->graphicsQueues[0]);
+    computeSection = createRenderSection(context, &context->computeQueues[0]);
     {
         VkDescriptorPoolSize poolSizes[] = {
             {VK_DESCRIPTOR_TYPE_STORAGE_IMAGE, 1},
@@ -90,37 +77,36 @@ void createResources(StromboliContext* context) {
         allocateInfo.pSetLayouts = &computePipeline.descriptorLayouts[0];
         vkAllocateDescriptorSets(context->device, &allocateInfo, &descriptorSet);
     }
-    {
+    for(u32 i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
         VkSemaphoreCreateInfo createInfo = {VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO};
-        vkCreateSemaphore(context->device, &createInfo, 0, &imageAcquireSemaphore);
+        vkCreateSemaphore(context->device, &createInfo, 0, &imageAcquireSemaphores[i]);
+        vkCreateSemaphore(context->device, &createInfo, 0, &imageReleaseSemaphores[i]);
     }
 }
 
 void destroyResources(StromboliContext* context) {
-    vkDestroyCommandPool(context->device, graphicsCommandPool, 0);
-    vkDestroyCommandPool(context->device, computeCommandPool, 0);
+    destroyRenderSection(context, &graphicsSection);
+    destroyRenderSection(context, &computeSection);
     vkDestroyDescriptorPool(context->device, descriptorPool, 0);
-    vkDestroySemaphore(context->device, imageAcquireSemaphore, 0);
+    for(u32 i = 0; i < MAX_FRAMES_IN_FLIGHT; ++i) {
+        vkDestroySemaphore(context->device, imageAcquireSemaphores[i], 0);
+        vkDestroySemaphore(context->device, imageReleaseSemaphores[i], 0);
+    }
     stromboliPipelineDestroy(context, &graphicsPipeline);
     stromboliRenderpassDestroy(context, &renderPass);
 }
 
 void renderComputeFrame(StromboliContext* context) {
     u32 imageIndex = 0;
-    VkResult acquireResult = vkAcquireNextImageKHR(context->device, swapchain.swapchain, UINT64_MAX, imageAcquireSemaphore, 0, &imageIndex);
+    static u32 frameIndex = 0;
+    VkCommandBuffer commandBuffer = beginRenderSection(context, &computeSection, frameIndex, "Compute");
+    VkResult acquireResult = vkAcquireNextImageKHR(context->device, swapchain.swapchain, UINT64_MAX, imageAcquireSemaphores[frameIndex], 0, &imageIndex);
     ASSERT(acquireResult == VK_SUCCESS);
-    
-    vkResetCommandPool(context->device, computeCommandPool, 0);
-    VkCommandBufferBeginInfo beginInfo = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
-    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
 
     StromboliDescriptorInfo set0[1];
     set0[0] = stromboliCreateImageDescriptor(VK_IMAGE_LAYOUT_GENERAL, swapchain.imageViews[imageIndex], 0);
     vkUpdateDescriptorSetWithTemplateKHR(context->device, descriptorSet, computePipeline.updateTemplates[0], set0);
-
-    vkBeginCommandBuffer(computeCommandBuffer, &beginInfo);
     {
-        VkCommandBuffer commandBuffer = computeCommandBuffer;
         { // Swapchain image Undefined -> General
             VkImageMemoryBarrier2KHR imageBarrier = stromboliCreateImageBarrier(swapchain.images[imageIndex],
             /*Src*/ VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT, 0, VK_IMAGE_LAYOUT_UNDEFINED,
@@ -141,72 +127,49 @@ void renderComputeFrame(StromboliContext* context) {
             stromboliPipelineBarrier(commandBuffer, 0, 0, 0, 1, &imageBarrier);
         }
     }
-    vkEndCommandBuffer(computeCommandBuffer);
-
-    VkSubmitInfo submitInfo = {VK_STRUCTURE_TYPE_SUBMIT_INFO};
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &computeCommandBuffer;
-    submitInfo.pWaitSemaphores = &imageAcquireSemaphore;
-    submitInfo.waitSemaphoreCount = 1;
     VkPipelineStageFlags waitMask = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
-    submitInfo.pWaitDstStageMask = &waitMask;
-    vkQueueSubmit(context->computeQueues[0].queue, 1, &submitInfo, 0);
-
-    //TODO:
-    vkDeviceWaitIdle(context->device);
+    endRenderSection(context, &computeSection, frameIndex, 1, &imageAcquireSemaphores[frameIndex], &waitMask, 1, &imageReleaseSemaphores[frameIndex]);
 
     VkPresentInfoKHR presentInfo = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR};
     presentInfo.pImageIndices = &imageIndex;
     presentInfo.swapchainCount = 1;
     presentInfo.pSwapchains = &swapchain.swapchain;
+    presentInfo.waitSemaphoreCount = 1;
+    presentInfo.pWaitSemaphores = &imageReleaseSemaphores[frameIndex];
     VkResult presentResult = vkQueuePresentKHR(context->computeQueues[0].queue, &presentInfo);
     ASSERT(presentResult == VK_SUCCESS);
+
+    frameIndex = (frameIndex+1) % MAX_FRAMES_IN_FLIGHT;
 }
 
 void renderGraphicsFrame(StromboliContext* context) {
+    static u32 frameIndex = 0;
     u32 imageIndex = 0;
-    VkResult acquireResult = vkAcquireNextImageKHR(context->device, swapchain.swapchain, UINT64_MAX, imageAcquireSemaphore, 0, &imageIndex);
-    ASSERT(acquireResult == VK_SUCCESS);
     
-    vkResetCommandPool(context->device, graphicsCommandPool, 0);
-    VkCommandBufferBeginInfo beginInfo = {VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO};
-    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-
-    vkBeginCommandBuffer(graphicsCommandBuffer, &beginInfo);
+    VkCommandBuffer commandBuffer = beginRenderSection(context, &graphicsSection, frameIndex, "Graphics");
     {
-        VkCommandBuffer commandBuffer = graphicsCommandBuffer;
-        VkRenderPassBeginInfo beginInfo = {VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO};
-        beginInfo.renderPass = renderPass.renderPass;
-        beginInfo.clearValueCount = renderPass.numClearColors;
-        beginInfo.pClearValues = renderPass.clearColors;
-        beginInfo.framebuffer = renderPass.framebuffers[imageIndex];
-        beginInfo.renderArea = (VkRect2D){.offset = {0.0f, 0.0f}, .extent = {swapchain.width, swapchain.height}};
+        VkResult acquireResult = vkAcquireNextImageKHR(context->device, swapchain.swapchain, UINT64_MAX, imageAcquireSemaphores[frameIndex], 0, &imageIndex);
+        ASSERT(acquireResult == VK_SUCCESS);
+
         stromboliCmdSetViewportAndScissor(commandBuffer, swapchain.width, swapchain.height);
-        vkCmdBeginRenderPass(commandBuffer, &beginInfo, VK_SUBPASS_CONTENTS_INLINE);
+        stromboliCmdBeginRenderpass(commandBuffer, &renderPass, swapchain.width, swapchain.height, imageIndex);
         vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, graphicsPipeline.pipeline);
         vkCmdDraw(commandBuffer, 3, 1, 0, 0);
         vkCmdEndRenderPass(commandBuffer);
     }
-    vkEndCommandBuffer(graphicsCommandBuffer);
-
-    VkSubmitInfo submitInfo = {VK_STRUCTURE_TYPE_SUBMIT_INFO};
-    submitInfo.commandBufferCount = 1;
-    submitInfo.pCommandBuffers = &graphicsCommandBuffer;
-    submitInfo.pWaitSemaphores = &imageAcquireSemaphore;
-    submitInfo.waitSemaphoreCount = 1;
     VkPipelineStageFlags waitMask = VK_PIPELINE_STAGE_COMPUTE_SHADER_BIT;
-    submitInfo.pWaitDstStageMask = &waitMask;
-    vkQueueSubmit(context->graphicsQueues[0].queue, 1, &submitInfo, 0);
-
-    //TODO:
-    vkDeviceWaitIdle(context->device);
+    endRenderSection(context, &graphicsSection, frameIndex, 1, &imageAcquireSemaphores[frameIndex], &waitMask, 1, &imageReleaseSemaphores[frameIndex]);
 
     VkPresentInfoKHR presentInfo = {VK_STRUCTURE_TYPE_PRESENT_INFO_KHR};
     presentInfo.pImageIndices = &imageIndex;
     presentInfo.swapchainCount = 1;
     presentInfo.pSwapchains = &swapchain.swapchain;
+    presentInfo.waitSemaphoreCount = 1;
+    presentInfo.pWaitSemaphores = &imageReleaseSemaphores[frameIndex];
     VkResult presentResult = vkQueuePresentKHR(context->graphicsQueues[0].queue, &presentInfo);
     ASSERT(presentResult == VK_SUCCESS);
+
+    frameIndex = (frameIndex+1) % MAX_FRAMES_IN_FLIGHT;
 }
 
 int main(int argc, char** argv) {
@@ -259,7 +222,8 @@ int main(int argc, char** argv) {
                 break;
             }
             if(events[i].type == GROUNDED_EVENT_TYPE_RESIZE) {
-                stromboliSwapchainResize(&context, &swapchain, VK_IMAGE_USAGE_STORAGE_BIT, events[i].resize.width, events[i].resize.height);
+                vkDeviceWaitIdle(context.device);
+                resizeApplication(&context, events[i].resize.width, events[i].resize.height);
             }
         }
 
